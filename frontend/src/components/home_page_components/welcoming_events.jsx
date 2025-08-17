d9901453df0df1c89858b3480d4959a04fb4dd40
Ansh Shama <soumya16kp@gmail.com>
2025-08-18 01:50:32 +0530

    renamed:    frontend/src/assets/ChatGPT Image Aug 17, 2025, 03_26_47 PM.png -> frontend/src/assets/ai.png 	modified:   frontend/src/components/home_page_components/welcoming_events.css 	modified:   frontend/src/components/home_page_components/welcoming_events.jsx

--- frontend/src/components/home_page_components/welcoming_events.jsx
+++ frontend/src/components/home_page_components/welcoming_events.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import './welcoming_events.css'
+import ai from "../../assets/ai.png";
 import front from "../../assets/front_photo.png";
+let i = 0;
 function WelcomingEvents() {
+    function Change(direction) {
+
+    }
     return(
         <>
             <div className="main">
-                <div className="arrow"><i class="fa-solid fa-angle-left"></i></div>
-                <div className="welcome-card">
+                <div className="arrow" onClick={Change("backward")}><i class="fa-solid fa-angle-left"></i></div>
+                <div className="welcome-card" id='box0'>
                     <div className="front_image">
                         <img src={front} />
                     </div>
@@ -17,28 +22,23 @@ function WelcomingEvents() {
                     </div>
                 </div>
                 <div className="box1">
-                                        <div className="front_image">
-                        <img src={front} />
+                    <div className="images">
+                        <img src={ai} />
                     </div>
-                    <div className="text">
+                    <div className="text" id="box1">
                         <h1>AI Horizons: Exploring the Future of Artificial Intelligence</h1>
                         <p>By AI Club<br />Date : 25th August 2025<br />Time: 4:00 PM - 6:30 PM (IST)<br />
-                        Dive into the world of Artificial Intelligence and Machine Learning with hands-on sessions,
+                        Dive into the world of Artificial Intelligence<br /> and Machine Learning with hands-on sessions,
                         <br /> Expert talks, 
-                        <br />And live demos.
-                        </p>
-                        <ul>
-                            <p>This event will cover</p>
-                            <li>Latest AI trends and technologies</li>
-                            <li>Hands-on sessions with AI tools</li>
-                            <li>Expert talks from industry leaders</li>
-                            <p>Perks</p>
-                            <li>Participation Certificate</li>
-                            <li>Networking Opportunities with AI Enthusiasts</li>
-                        </ul>
+                        <br />And live demos.</p>
+                        <p>This event will cover</p>
+                        <li>Latest AI trends and technologies</li>
+                        <li>Hands-on sessions with AI tools</li>
+                        <li>Expert talks from industry leaders</li>
+                        <li>Networking opportunities with AI enthusiasts</li>
                     </div>
                 </div>
-                <div className="arrow"><i class="fa-solid fa-angle-right"></i></div>
+                <div className="arrow" onClick={Change("forward")}><i class="fa-solid fa-angle-right"></i></div>
             </div>
         </>
     )
