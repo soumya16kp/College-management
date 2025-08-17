87a6bc58c7557c11a538ba88a1a67098093926a2
Ansh Shama <soumya16kp@gmail.com>
2025-08-17 15:29:28 +0530

    new file:   frontend/src/assets/ChatGPT Image Aug 17, 2025, 03_26_47 PM.png 	modified:   frontend/src/boxes/Header/Header.css 	modified:   frontend/src/components/home_page_components/welcoming_events.css 	modified:   frontend/src/components/home_page_components/welcoming_events.jsx

--- frontend/src/components/home_page_components/welcoming_events.jsx
+++ frontend/src/components/home_page_components/welcoming_events.jsx
@@ -16,6 +16,28 @@ function WelcomingEvents() {
                             Join events, and <br /> Connect with your community.</p>
                     </div>
                 </div>
+                <div className="box1">
+                                        <div className="front_image">
+                        <img src={front} />
+                    </div>
+                    <div className="text">
+                        <h1>AI Horizons: Exploring the Future of Artificial Intelligence</h1>
+                        <p>By AI Club<br />Date : 25th August 2025<br />Time: 4:00 PM - 6:30 PM (IST)<br />
+                        Dive into the world of Artificial Intelligence and Machine Learning with hands-on sessions,
+                        <br /> Expert talks, 
+                        <br />And live demos.
+                        </p>
+                        <ul>
+                            <p>This event will cover</p>
+                            <li>Latest AI trends and technologies</li>
+                            <li>Hands-on sessions with AI tools</li>
+                            <li>Expert talks from industry leaders</li>
+                            <p>Perks</p>
+                            <li>Participation Certificate</li>
+                            <li>Networking Opportunities with AI Enthusiasts</li>
+                        </ul>
+                    </div>
+                </div>
                 <div className="arrow"><i class="fa-solid fa-angle-right"></i></div>
             </div>
         </>
