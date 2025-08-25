d1cab61eb015b36f43a901a0df62f07626855d05
Ansh Shama <soumya16kp@gmail.com>
2025-08-25 20:13:35 +0530

    modified:   frontend/src/components/sreach/searching.css 	modified:   frontend/src/components/sreach/sreaching.jsx

--- frontend/src/components/sreach/sreaching.jsx
+++ frontend/src/components/sreach/sreaching.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import "./searching.css";
 
 const Searching = () => {
+  const [showOptions, setShowOptions] = useState(false);
+
+  const toggleFilterOptions = () => {
+    setShowOptions(prev => !prev);
+  };
+
   return (
     <div className="search-container">
-      <input className="search" placeholder="search Amazon" />
+      <input className="search" placeholder="Search Clubs" />
       <div className="se-icon">
-        <i className="fa-solid fa-magnifying-glass"></i>
+        <button><i className="fa-solid fa-magnifying-glass"></i></button>
       </div>
-            <div className="filter-icon">
-        <i className="fa-solid fa-filter"></i>
+      <div className="filter-container">
+        <button className="filter-button" onClick={toggleFilterOptions}>
+          <i className="fa-solid fa-filter"></i>
+        </button>
+        {showOptions && (
+          <div className="sort-container">
+            <button className="sort-by-students">Sort by Students</button>
+            <button className="sort-by-year">Sort by Year</button>
+            <button className="sort-by-department">Sort by Department</button>
+          </div>
+        )}
       </div>
     </div>
   );
