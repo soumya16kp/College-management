223bc9028d59fd9752e7fd7e7f73235a2a82d0e7
Ansh Shama <soumya16kp@gmail.com>
2025-10-03 22:26:25 +0530

    modified:   potfinder/potfinder/asgi.py

--- potfinder/potfinder/asgi.py
+++ potfinder/potfinder/asgi.py
@@ -3,7 +3,7 @@ import django
 from django.core.asgi import get_asgi_application
 
 # Set Django settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'potfinder.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'potfinder.potfinder.settings')
 django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
